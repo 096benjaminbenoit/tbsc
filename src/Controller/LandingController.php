5de7ec0c678f8070b1aeeb41a65985fce2879963
Benjamin Benoit <benjaminbenoit@MacBook-Pro-de-Benjamin.local>
2023-03-22 14:27:50 +0100

    bdd et affichage des shows

--- src/Controller/LandingController.php
+++ src/Controller/LandingController.php
@@ -2,17 +2,22 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Repository\ShowsRepository;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class LandingController extends AbstractController
 {
-    #[Route('/  ', name: 'app_landing')]
-    public function index(): Response
+    #[Route('/', name: 'app_landing')]
+    public function index(ShowsRepository $showsRepository, Request $request): Response
     {
+        $shows = $showsRepository->findAll();
+
         return $this->render('landing/index.html.twig', [
             'controller_name' => 'LandingController',
+            'shows' => $shows,
         ]);
     }
 }
---
+++ src/Entity/Shows.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\ShowsRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: ShowsRepository::class)]
+class Shows
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $name = null;
+
+    #[ORM\Column(type: Types::DATE_MUTABLE)]
+    
+    private ?\DateTimeInterface $date = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $place = null;
+
+    public function __toString()
+    {
+        return $this->name;
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getDate(): ?\DateTimeInterface
+    {
+        return $this->date;
+    }
+
+    public function setDate(\DateTimeInterface $date): self
+    {
+        $this->date = $date;
+
+        return $this;
+    }
+
+    public function getPlace(): ?string
+    {
+        return $this->place;
+    }
+
+    public function setPlace(string $place): self
+    {
+        $this->place = $place;
+
+        return $this;
+    }
+}
